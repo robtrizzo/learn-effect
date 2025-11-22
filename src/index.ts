4d9b629092d15fbf9bfed01ba180691fed34f16e
Robert Rizzo <robtrizzo@gmail.com>
2025-11-21 20:54:10 -0500

    custom errors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,37 @@
-import {Console, Effect} from "effect";
+import { Data, Effect } from "effect";
 
-const main = Console.log("Hello, World!")
+interface FetchError {
+    readonly _tag: "FetchError";
+}
+class FetchError extends Data.TaggedError("FetchError")<{}> { }
 
-Effect.runSync(main)
+
+interface JsonError {
+    readonly _tag: "JsonError"
+}
+class JsonError extends Data.TaggedError("JsonError")<{}> { }
+
+
+const fetchRequest = Effect.tryPromise({
+    try: () => fetch("https://pokeapi.co/api/v2/pokemon/garchomp/"),
+    catch: () => new FetchError()
+});
+
+const jsonResponse = (response: Response) => Effect.tryPromise({
+    try: () => response.json(),
+    catch: () => new JsonError()
+});
+
+const main = fetchRequest.pipe(
+    Effect.filterOrFail(
+        (response) => response.ok,
+        () => new FetchError()
+    ),
+    Effect.flatMap(jsonResponse),
+    Effect.catchTags({
+        FetchError: () => Effect.succeed("Fetch error"),
+        JsonError: () => Effect.succeed("Json error"),
+    })
+)
+
+Effect.runPromise(main);
